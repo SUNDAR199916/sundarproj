7ba8cf67678df88312de2e4bdb17a8ac936c00a2
SUNDAR199916 <sundard471@gmail.com>
2024-09-04 12:54:24 +0530

    https://github.com/SUNDAR199916/sundarproj.git

---
+++ src/org/exception/Exception.java
@@ -0,0 +1,82 @@
+package org.exception;
+
+public class Exception {
+	
+      public static void main(String[] args) {
+
+    	  try
+          {
+           System.out.printf("1");
+            int data = 5 / 0;
+          }
+         catch(ArithmeticException e)
+          {
+         System.out.printf("2");
+         System.exit(0);
+           }
+         finally
+            {
+          System.out.printf("3");
+            }
+         System.out.printf("4");
+           }
+            }
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	  
+    	 // try {
+           //   System.out.printf("1");
+             // int sum = 9 / 0;
+               
+           //   System.out.printf("2");
+               
+         //  } catch (ArithmeticException e) {
+             //  System.out.printf("3");
+           
+         //  } finally {
+           //    System.out.printf("5");
+       //    }
+     //  }
+   //}  
+    		
+    		
+    	 
+          
+          
+     
+
+
